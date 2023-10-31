289a9cdd2cb10422efde0b56c945549a69f6d10d
Mevlüt Can Turacı <53406778+MevlutCanTuraci@users.noreply.github.com>
2023-10-31 22:36:51 +0300

    Init

---
+++ PasswordHasher/PasswordHasher/PasswordHasherHelper.cs
@@ -0,0 +1,56 @@
+﻿using System.Security.Cryptography;
+
+namespace PasswordHasher
+{
+    public class PasswordHasherHelper
+    {
+        private const int _SaltSize      = 128 / 8;
+        private const int _KeySize       = 256 / 8;
+        private const int _Iterations    = 10000;
+        private readonly HashAlgorithmName _hashAlgorithmName = HashAlgorithmName.SHA256;
+
+        public byte[] GenerateHash(byte[] salt, string password, out string hashedPass)
+        {
+            var hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, _Iterations, _hashAlgorithmName, _KeySize);
+            hashedPass = Convert.ToBase64String(hash);
+            return hash;
+        }
+
+
+        public byte[] GenerateSalt(out string salted)
+        {
+            var salt = RandomNumberGenerator.GetBytes(_SaltSize);
+            salted = Convert.ToBase64String(salt);
+            return salt;
+        }
+
+
+        public (string Hash, string Salt) CreateHash(string password)
+        {
+            string saltString = string.Empty;
+            byte[] salt = GenerateSalt(out saltString);
+
+            string hashString = string.Empty;
+            byte[] hash = GenerateHash(salt, password ,out hashString);
+
+            return (hashString, saltString);
+        }
+
+
+        public bool VerifyPassword(byte[] salt, byte[] hash, string inputPassword)
+        {
+            var inputHash = GenerateHash(salt, inputPassword, out _);
+            return CryptographicOperations.FixedTimeEquals(hash, inputHash);
+        }
+
+
+        public bool VerifyPassword(string salt, string hash, string inputPassword)
+        {
+            byte[] saltByte = Convert.FromBase64String(salt);
+            byte[] hashByte = Convert.FromBase64String(hash);
+
+            var inputHash = GenerateHash(saltByte, inputPassword, out _);
+            return CryptographicOperations.FixedTimeEquals(hashByte, inputHash);
+        }
+    }
+}
---
+++ PasswordHasher/PasswordHasher/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Data.SqlTypes;
+
+namespace PasswordHasher
+{
+    public class Program
+	{
+        static PasswordHasherHelper _helper = new PasswordHasherHelper();
+
+        public static void Main()
+        {
+            #region Password hash generator function #1
+
+            //GenerateHash();
+
+            #endregion
+
+
+            #region Password hash generator function #2
+
+            GenerateHashShort();
+
+            #endregion
+
+
+            #region Password verify function
+
+            //! Password is 1234
+            //VerifyPasswordHash();
+
+            #endregion
+
+            Console.Read();
+        }
+
+
+        private static void GenerateHash()
+        {
+            string myPassword = "1234";
+            string saltString = string.Empty, hashString = string.Empty;
+
+            byte[] salt = _helper.GenerateSalt(out saltString);
+            byte[] hash = _helper.GenerateHash(salt, myPassword, out hashString);
+
+            Console.WriteLine($"Your password: \t{myPassword}\r\nYour salt: \t{saltString}\r\nYour hash: \t{hashString}");
+        }
+
+
+        private static void GenerateHashShort()
+        {
+            string myPassword = "1234";
+            (string Salt, string Hash) hasherResult = _helper.CreateHash(myPassword);
+            
+            Console.WriteLine($"Your password: \t{myPassword}\r\nYour salt: \t{hasherResult.Salt}\r\nYour hash: \t{hasherResult.Hash}");
+        }
+
+
+        private static void VerifyPasswordHash()
+        {
+            //string myPassword = "1234";
+            string saltString = "lUZbZisyHShnCKm/E3HIkw==", hashString = "f60HgyRN9TIgGOB7OfPT6Ke4Vxz36Ccsa/p8+OJuLuw=";
+
+            Console.Write("Enter password: ");
+            string userPassword = Console.ReadLine();
+
+            #region Using #1
+
+            byte[] salt = Convert.FromBase64String(saltString);
+            byte[] hash = Convert.FromBase64String(hashString);
+
+            bool isVerifyPassword1 = _helper.VerifyPassword(salt, hash, userPassword);
+
+            #endregion
+
+
+            #region Using #2 
+
+            bool isVerifyPassword2 = _helper.VerifyPassword(saltString, hashString, userPassword);
+
+            #endregion
+
+            if (isVerifyPassword1)
+            {
+                Console.WriteLine(new string('*', 20));
+                Console.WriteLine("(1)-Password is Correct.");
+            }
+            if (isVerifyPassword2)
+            {
+                Console.WriteLine(new string('*', 20));
+                Console.WriteLine("(2)-Password is Correct.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Password!");
+            }
+        }
+
+    }
+}
